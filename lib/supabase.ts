e37976e398138c90a1310d77d78c95c578b9cc8b
Roman Fiala <fiala@vinaria.cz>
2024-11-27 18:57:26 +0100

    Move Supabase client initialization to lib folder

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.https://uhawlwolmyoqcdurhuel.supabase.co
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVoYXdsd29sbXlvcWNkdXJodWVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIyNzgxMDUsImV4cCI6MjA0Nzg1NDEwNX0.2o5fFfo1q3xMKjD7QfFNYcsWb8zv5peWsbFLtnJQF4Y
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
